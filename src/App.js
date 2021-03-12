3a1f1850fd77305f2df5e0b0c58f906bac694306
bill-c2018 <wcunning81@gmail.com>
2021-03-12 10:37:19 -0800

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
 
-import './App.css';
+import './MyApp.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-
-
-      </header>
-    </div>
-  );
+		<>
+		<div className="page-wrapper" style={{backgroundColor: "#f1f1f1"}}>
+			<div className="row">
+				<div className="column-side2" style={{backgroundColor: "#aaa", height: '80%'}}>
+					logo
+				</div>
+				<div className="header">
+					<h2>Header</h2>
+				</div>	
+			</div>		
+			<div className="row">
+				<div className="column-side" style={{backgroundColor: "#aaa", height: '80%'}}>
+					nav
+				</div>
+				<div className="column-middle" >
+					content
+				</div>
+			</div>
+			<div className="footer">
+				<p>Footer</p>
+			</div>
+		</div>
+		</>
+	);
 }
 
 export default App;
